c94ce2579cc453128b319786c05816fce6325d83
Jason Hirata <jhirata@integrity-apps.com>
2017-10-08 19:27:19 -1000

    removing console log

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ export default function(opts, data) {
 	xhr.onreadystatechange = function() {
 	    if(xhr.readyState === 4) {
 		if(xhr.status === 200 || xhr.status === 201) {
-		    console.log("%s resolved with status %s", opts.url, xhr.status);
+//		    console.log("%s resolved with status %s", opts.url, xhr.status);
 		    resolve(xhr.responseText);
 		}
 		else {
-		    console.log("rejected message to %s with status %s", opts.url, xhr.status);
+//		    console.log("rejected message to %s with status %s", opts.url, xhr.status);
 		    reject();
 		}
 		
